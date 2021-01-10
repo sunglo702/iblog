c63cf985d230083879ef699ed505b481f6a00717
sunglo702 <hanxuefeng131@163.com>
2021-01-10 16:58:14 +0800

    complete detail for post

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -13,6 +15,11 @@ class Category(models.Model):
     https://docs.djangoproject.com/en/2.2/ref/models/fields/#field-types
     """
     name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
@@ -22,44 +29,38 @@ class Tag(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    """
-    文章的数据库表稍微复杂一点，主要是涉及的字段更多。
-    """
 
-    # 文章标题
-    title = models.CharField(max_length=70)
-
-    # 文章正文，我们使用了 TextField。
-    # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
-    body = models.TextField()
-
-    # 这两个列分别表示文章的创建时间和最后一次修改时间，存储时间的字段用 DateTimeField 类型。
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
-
-    # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
-    # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
-    excerpt = models.CharField(max_length=200, blank=True)
-
-    # 这是分类与标签，分类与标签的模型我们已经定义在上面。
-    # 我们在这里把文章对应的数据库表和分类、标签对应的数据库表关联了起来，但是关联形式稍微有点不同。
-    # 我们规定一篇文章只能对应一个分类，但是一个分类下可以有多篇文章，所以我们使用的是 ForeignKey，即一
-    # 对多的关联关系。且自 django 2.0 以后，ForeignKey 必须传入一个 on_delete 参数用来指定当关联的
-    # 数据被删除时，被关联的数据的行为，我们这里假定当某个分类被删除时，该分类下全部文章也同时被删除，因此     # 使用 models.CASCADE 参数，意为级联删除。
-    # 而对于标签来说，一篇文章可以有多个标签，同一个标签下也可能有多篇文章，所以我们使用
-    # ManyToManyField，表明这是多对多的关联关系。
-    # 同时我们规定文章可以没有标签，因此为标签 tags 指定了 blank=True。
-    # 如果你对 ForeignKey、ManyToManyField 不了解，请看教程中的解释，亦可参考官方文档：
-    # https://docs.djangoproject.com/en/2.2/topics/db/models/#relationships
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-
-    # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
-    # django.contrib.auth 是 django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是
-    # django 为我们已经写好的用户模型。
-    # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
-    # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和
-    # Category 类似。
+    title = models.CharField('标题', max_length=70)
+    body = models.TextField('正文')
+    created_time = models.DateTimeField('创建时间')
+    modified_time = models.DateTimeField('修改时间')
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
+
+    # 自定义 get_absolute_url 方法
+    # 记得从 django.urls 中导入 reverse 函数
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
--- django/blogproject/settings.py
+++ django/blogproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'blog',
+	'blog.apps.BlogConfig',  # 注册 blog 应用
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blogproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# 把英文改为中文
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+# 把国际时区改为中国时区（东八区）
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
